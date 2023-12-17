c428a3138dbdced22a28e4253d608938d97be1d4
PokeFred <cedric.paelmke@gmx.de>
2023-12-17 21:26:13 +0100

    add FontAwesome

--- src/app.svelte
+++ src/app.svelte
@@ -1,1 +1,6 @@
-<div>Hello World</div>
+<script lang="ts">
+    import Icon from "svelte-awesome"
+    import {faThumbsUp} from "@fortawesome/free-solid-svg-icons/faThumbsUp"
+</script>
+
+<div>Hello World <Icon data={faThumbsUp} /></div>
